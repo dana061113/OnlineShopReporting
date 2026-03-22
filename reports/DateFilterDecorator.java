a267d44444645ebc3c55a4a9cf4eeac14b3f43ae
dana061113 <danakobylandykyzy@gmail.com>
2026-03-22 20:07:47 +0500

    Create DateFilterDecorator.java

---
+++ reports/DateFilterDecorator.java
@@ -0,0 +1,17 @@
+package reports;
+
+public class DateFilterDecorator extends ReportDecorator {
+    private String startDate;
+    private String endDate;
+
+    public DateFilterDecorator(IReport report, String startDate, String endDate) {
+        super(report);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String generate() {
+        return report.generate() + " | Фильтр по датам: " + startDate + " - " + endDate;
+    }
+}
